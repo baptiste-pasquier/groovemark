10c2af0d3b6d9177064e1af1aa46bfb6c1679382
Baptiste Pasquier <pasquierbaptiste@hotmail.fr>
2025-12-20 20:05:16 +0100

    feat: update YouTube video ID regex to support live video URLs

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,7 +2,7 @@ import pb from '../services/pocketbase'
 
 export function getYoutubeVideoId(url: string): string | null {
   const regex =
-    /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+    /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?|live)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
   const match = url.match(regex)
   return match ? match[1] : null
 }
